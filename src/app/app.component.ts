3a6124a1b20126c5a99c2719e8f9be8982f25a61
Ziyatsi Fanye Rocha Guerrero <ziyatsir@gmail.com>
2024-09-17 21:06:11 -0600

    primer commit martes 17

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'basesAngular';
+  duplicarNumero(x:number):number{
+    return x*2;
+  }
+  alumno={
+    matricula:21000532,
+    nombre:'Ye',
+    fechaInscrito:new Date(),
+    pago:2220
+  };
+}
